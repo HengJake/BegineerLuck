945560eb92476286b417a843c5532ad4588897a3
Heng Jun Kai <hengjunkai@gmail.com>
2025-04-27 21:34:02 +0800

    login page frontend

---
+++ NFT_MarketPlace/LoginPage/Login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="\BeginnerLuck_MarketPlace\BegineerLuck_WebDev\NFT_MarketPlace\general.css">
+    <title>Marketplace Log In</title>
+</head>
+
+<body class="Login_Body">
+    <div class="Login_BackgroundImage"></div>
+    <div class="flex-column flex-center">
+        <h2 class="Login_Title text-center mb-3">Login</h2>
+        <div class="Login_Container flex-center flex-column">
+            <form class="w-100">
+                <p class="Login_SmallText w-100 text-left mb-3">Don't have an account yet? <a href="">Sign Up</a></p>
+                <input class="w-100 mb-2" type="text" placeholder="Username" required>
+                <input class="w-100 mb-1" type="password" placeholder="Password" required>
+                <p class="Login_SmallText mb-3"><a href="">Forget Password</a></p>
+                <button class="btn w-100" type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
