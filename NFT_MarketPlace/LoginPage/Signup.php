ec7033db140849385e3238cc30f06f326ec3305d
Heng Jun Kai <hengjunkai@gmail.com>
2025-04-28 21:37:31 +0800

    test

--- NFT_MarketPlace/LoginPage/Signup.php
+++ NFT_MarketPlace/LoginPage/Signup.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="\BeginnerLuck_MarketPlace\BegineerLuck_WebDev\NFT_MarketPlace\general.css">
-    <title>Stickman Odyssey: Log In</title>
+    <title>Stickman Odyssey: Sign Up</title>
 </head>
 
 <body class="Login_Body">
     <div class="Login_BackgroundImage"></div>
     <div class="flex-column flex-center">
-        <h2 class="Login_Title text-center mb-3">Login</h2>
+        <h2 class="Login_Title text-center mb-3">Sign Up</h2>
         <div class="Login_Container flex-center flex-column">
             <form class="w-100">
-                <p class="Login_SmallText w-100 text-left mb-3">Don't have an account yet? <a href="\BeginnerLuck_MarketPlace\BegineerLuck_WebDev\NFT_MarketPlace\LoginPage\Signup.php">Sign Up</a></p>
+                <p class="Login_SmallText w-100 text-left mb-3">Already have an account? <a href="\BeginnerLuck_MarketPlace\BegineerLuck_WebDev\NFT_MarketPlace\LoginPage\Login.php">Log In</a></p>
                 <input class="w-100 mb-2" type="text" placeholder="Username" required>
                 <input class="w-100 mb-1" type="password" placeholder="Password" required>
                 <p class="Login_SmallText mb-3"><a href="">Forget Password</a></p>
