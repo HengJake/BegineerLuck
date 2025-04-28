9ef8f9d2d1519ebad07d74c782413ba68f9170a5
Heng Jun Kai <hengjunkai@gmail.com>
2025-04-28 21:01:40 +0800

    nft book layout

---
+++ NFT_MarketPlace/Homepage/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/BeginnerLuck_MarketPlace/BegineerLuck_WebDev/NFT_MarketPlace/general.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <title>Marketplace</title>
+</head>
+
+<body>
+    <div class="Homepage_Background"></div>
+    <header class="Homepage_Header">
+        <div class="Homepage_Directory">
+            <h1 class="Homepage_Title">[name] Marketplace</h1>
+            <input type="text" placeholder="Search...">
+            <div class="flex">
+
+                <a href="\BeginnerLuck_MarketPlace\BegineerLuck_WebDev\NFT_MarketPlace\ProfilePage\Profile.php">
+                    <span class="material-icons-outlined">
+                        account_circle
+                    </span>
+                </a>
+
+                <a href="\BeginnerLuck_MarketPlace\BegineerLuck_WebDev\NFT_MarketPlace\NFTDetailsPage\NFTDetails.php">
+                    <span class="material-icons-outlined">
+                        inventory_2
+                    </span>
+                </a>
+
+            </div>
+        </div>
+        <div class="Homepage_ShadeContainer">
+            <div class="Homepage__ShadeThingie Red"></div>
+            <div class="Homepage__ShadeThingie White"></div>
+            <div class="Homepage__ShadeThingie Red"></div>
+            <div class="Homepage__ShadeThingie White"></div>
+            <div class="Homepage__ShadeThingie Red"></div>
+            <div class="Homepage__ShadeThingie White"></div>
+            <div class="Homepage__ShadeThingie Red"></div>
+            <div class="Homepage__ShadeThingie White"></div>
+            <div class="Homepage__ShadeThingie Red"></div>
+            <div class="Homepage__ShadeThingie White"></div>
+            <div class="Homepage__ShadeThingie Red"></div>
+            <div class="Homepage__ShadeThingie White"></div>
+        </div>
+    </header>
+    <main>
+        <div></div>
+    </main>
+</body>
+
+</html>
--- NFT_MarketPlace/NFTDetailsPage/NFTDetails.php
+++ NFT_MarketPlace/NFTDetailsPage/NFTDetails.php
@@ -1,1 +1,33 @@
-asdasdasddasd
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/BeginnerLuck_MarketPlace/BegineerLuck_WebDev/NFT_MarketPlace/general.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <title>View Inventory</title>
+</head>
+
+<body class="Details_Body">
+    <main>
+        <button class="Details_Button back">
+            <span class="material-icons-outlined">
+                chevron_left
+            </span></button>
+        <button class="Details_Button foward">
+            <span class="material-icons-outlined">
+                navigate_next
+            </span>
+        </button>
+        <div class="Details_BookPage">
+            <h1>[player]'s NFT Collection</h1>
+        </div>
+        <div class="Details_BookPage">
+            <h1>NFT Details</h1>
+        </div>
+    </main>
+</body>
+<script src="NFTDetails.js"></script>
+
+</html>
