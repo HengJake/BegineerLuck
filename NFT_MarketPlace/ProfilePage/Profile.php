8ac44db1bd4d3b0495fc5fbcdd8faf682a4f44ca
Heng Jun Kai <hengjunkai@gmail.com>
2025-05-01 20:32:41 +0800

    logot button

---
+++ NFT_MarketPlace/ProfilePage/Profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <link rel="stylesheet" href="\BegineerLuck_WebDev\NFT_MarketPlace\general.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+
+    <button>Log Out</button>
+
+</body>
+
+</html>
